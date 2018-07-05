604d86092f8be846d24e907c679318ca20ed755f
Itbankrock <john_andrew_santiago@dlsu.edu.ph>
2018-07-06 02:23:45 +0800

    Hello, adapter done

--- app/src/main/java/com/example/leebet_pc/bidconnect/HomeActivity.java
+++ app/src/main/java/com/example/leebet_pc/bidconnect/HomeActivity.java
@@ -2,10 +2,20 @@ package com.example.leebet_pc.bidconnect;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.Window;
 import android.view.WindowManager;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class HomeActivity extends AppCompatActivity {
+
+    private List<Bid> movieList = new ArrayList<>();
+    private RecyclerView recyclerView;
+    private bidsAdapter bAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,5 +28,44 @@ public class HomeActivity extends AppCompatActivity {
             window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
             window.setStatusBarColor(this.getResources().getColor(R.color.status_color));
         }
+
+        recyclerView = (RecyclerView) findViewById(R.id.bids_recycler);
+
+        bAdapter = new bidsAdapter(movieList);
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false);
+        //RecyclerView.LayoutManager mLayoutManager = new LinearLayoutManager(getApplicationContext());
+        recyclerView.setLayoutManager(layoutManager);
+        recyclerView.setItemAnimator(new DefaultItemAnimator());
+        recyclerView.setAdapter(bAdapter);
+        prepareMovieData();
+    }
+
+    private void prepareMovieData() {
+        //public Bid(String username, String title, String views, String timer, String timestamp, String currbid, String buyoutprice)
+        Bid movie = new Bid("elisoriano","GALASYA 3:3","2.6k","1:33:00","11 hours ago","Current bid: 350.0 PHP","Current bid: 350.0 PHP");
+        movieList.add(movie);
+
+        movie = new Bid("elisoriano","GALASYA 3:3","2.6k","1:33:00","11 hours ago","Current bid: 350.0 PHP","Current bid: 350.0 PHP");
+        movieList.add(movie);
+
+        movie = new Bid("elisoriano","GALASYA 3:3","2.6k","1:33:00","11 hours ago","Current bid: 350.0 PHP","Current bid: 350.0 PHP");
+        movieList.add(movie);
+
+        movie = new Bid("elisoriano","GALASYA 3:3","2.6k","1:33:00","11 hours ago","Current bid: 350.0 PHP","Current bid: 350.0 PHP");
+        movieList.add(movie);
+
+        movie = new Bid("elisoriano","GALASYA 3:3","2.6k","1:33:00","11 hours ago","Current bid: 350.0 PHP","Current bid: 350.0 PHP");
+        movieList.add(movie);
+
+        movie = new Bid("elisoriano","GALASYA 3:3","2.6k","1:33:00","11 hours ago","Current bid: 350.0 PHP","Current bid: 350.0 PHP");
+        movieList.add(movie);
+
+        movie = new Bid("elisoriano","GALASYA 3:3","2.6k","1:33:00","11 hours ago","Current bid: 350.0 PHP","Current bid: 350.0 PHP");
+        movieList.add(movie);
+
+        movie = new Bid("elisoriano","GALASYA 3:3","2.6k","1:33:00","11 hours ago","Current bid: 350.0 PHP","Current bid: 350.0 PHP");
+        movieList.add(movie);
+
+        bAdapter.notifyDataSetChanged();
     }
 }
