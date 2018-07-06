5445624c1e19496ff4905b7ec0b5e0d4cf8510df
nandarionndr1 <leebet16@gmail.com>
2018-07-06 12:04:11 +0800

    "adding my auction pages"

---
+++ app/src/main/java/com/example/leebet_pc/bidconnect/AuctionActivity.java
@@ -0,0 +1,46 @@
+package com.example.leebet_pc.bidconnect;
+
+import android.support.design.widget.TabLayout;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+
+import android.widget.TextView;
+
+public class AuctionActivity extends AppCompatActivity {
+
+    private ViewPager mViewPager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_auction);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        // Set up the ViewPager with the sections adapter.
+        mViewPager = (ViewPager) findViewById(R.id.container);
+
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.tabs);
+
+
+
+    }
+
+
+
+}
---
+++ app/src/main/java/com/example/leebet_pc/bidconnect/BiddingFragment.java
@@ -0,0 +1,31 @@
+package com.example.leebet_pc.bidconnect;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class BiddingFragment extends Fragment {
+    private static final String TAG = "Bidding Fragement";
+
+    private Button btnTEST;
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.bidding_fragment,container,false);
+        btnTEST = (Button) view.findViewById(R.id.button_bidding);
+
+        btnTEST.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getActivity(), "TESTING BUTTON CLICK 1", Toast.LENGTH_SHORT).show();
+            }
+        });
+        return view;
+    }
+}
