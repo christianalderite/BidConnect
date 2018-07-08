185a7d6e74018ad29dbb6f78d3f98095b3c3aff2
nandarionndr1 <leebet16@gmail.com>
2018-07-08 19:26:11 +0800

    fixed layout for andrew

--- app/src/main/java/com/example/leebet_pc/bidconnect/Account.java
+++ app/src/main/java/com/example/leebet_pc/bidconnect/Account.java
@@ -90,17 +90,12 @@ public class Account extends AppCompatActivity {
             }
         });
 
-
-
-
-
         if (android.os.Build.VERSION.SDK_INT >= 21) {
             Window window = this.getWindow();
             window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
             window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
             window.setStatusBarColor(this.getResources().getColor(R.color.status_color));
         }
-
         recyclerView = (RecyclerView) findViewById(R.id.bids_recycler);
 
         bAdapter = new bidsAdapter(2,biddingItems);
@@ -117,30 +112,15 @@ public class Account extends AppCompatActivity {
     }
 
     private void prepareBiddingItems() {
-        //public Bid(String username, String title, String views, String timer, String timestamp, String currbid, String buyoutprice)
+
         Bid movie = new Bid("elisoriano","GALASYA 3:3","2.6k","1:33:00","11 hours ago","Current bid: 350.0 PHP","Current bid: 350.0 PHP","");
         biddingItems.add(movie);
 
         movie = new Bid("elisoriano","GALASYA 3:3","2.6k","1:33:00","11 hours ago","Current bid: 350.0 PHP","Current bid: 350.0 PHP","");
         biddingItems.add(movie);
 
-        movie = new Bid("elisoriano","GALASYA 3:3","2.6k","1:33:00","11 hours ago","Current bid: 350.0 PHP","Current bid: 350.0 PHP","");
-        biddingItems.add(movie);
 
-        movie = new Bid("elisoriano","GALASYA 3:3","2.6k","1:33:00","11 hours ago","Current bid: 350.0 PHP","Current bid: 350.0 PHP","");
-        biddingItems.add(movie);
-
-        movie = new Bid("elisoriano","GALASYA 3:3","2.6k","1:33:00","11 hours ago","Current bid: 350.0 PHP","Current bid: 350.0 PHP","");
-        biddingItems.add(movie);
-
-        movie = new Bid("elisoriano","GALASYA 3:3","2.6k","1:33:00","11 hours ago","Current bid: 350.0 PHP","Current bid: 350.0 PHP","");
-        biddingItems.add(movie);
-
-        movie = new Bid("elisoriano","GALASYA 3:3","2.6k","1:33:00","11 hours ago","Current bid: 350.0 PHP","Current bid: 350.0 PHP","");
-        biddingItems.add(movie);
-
-        movie = new Bid("elisoriano","GALASYA 3:3","2.6k","1:33:00","11 hours ago","Current bid: 350.0 PHP","Current bid: 350.0 PHP","");
-        biddingItems.add(movie);
+        //RETRIEVE BIDDING ITEMS HERE
 
         bAdapter.notifyDataSetChanged();
     }
