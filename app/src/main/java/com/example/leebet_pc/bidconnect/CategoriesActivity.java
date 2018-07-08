1d2a3e5bca00ebd39542da6d62e7a3cddd894d80
nandarionndr1 <leebet16@gmail.com>
2018-07-08 15:37:52 +0800

    search category skeleton

--- app/src/main/java/com/example/leebet_pc/bidconnect/CategoriesActivity.java
+++ app/src/main/java/com/example/leebet_pc/bidconnect/CategoriesActivity.java
@@ -2,12 +2,21 @@ package com.example.leebet_pc.bidconnect;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class CategoriesActivity extends AppCompatActivity {
-
+    Button back;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_categories);
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+
     }
 }
