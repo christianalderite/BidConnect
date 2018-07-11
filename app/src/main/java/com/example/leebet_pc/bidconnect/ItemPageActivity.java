17a4e1753582f2bcc4e6282c5482dd549a24dc24
nandarionndr1 <leebet16@gmail.com>
2018-07-11 22:24:08 +0800

    andrew d dog

--- app/src/main/java/com/example/leebet_pc/bidconnect/ItemPageActivity.java
+++ app/src/main/java/com/example/leebet_pc/bidconnect/ItemPageActivity.java
@@ -348,7 +348,7 @@ public class ItemPageActivity extends AppCompatActivity {
 
         updateHighestBid();
 
-        dbComments.orderByChild("timestamp").addListenerForSingleValueEvent(new ValueEventListener() {
+        dbComments.orderByChild("timestamp").addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
 
@@ -370,30 +370,7 @@ public class ItemPageActivity extends AppCompatActivity {
 
             }
         });
-        dbComments.orderByChild("timestamp").limitToLast(1).addValueEventListener(new ValueEventListener() {
-            @Override
-            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
 
-                Collections.reverse(commentList);
-                for (DataSnapshot object: dataSnapshot.getChildren()){
-                    AuctionComment zucc = object.getValue(AuctionComment.class);
-                    String a = receiveAuction.getAuctionID();
-                    String b =  zucc.getAuctionID();
-                    if(a.equals(b)){
-                        commentList.add(zucc);
-
-                    }
-                }
-
-                Collections.reverse(commentList);
-                CommentAuctionAdapter.notifyDataSetChanged();
-            }
-
-            @Override
-            public void onCancelled(@NonNull DatabaseError databaseError) {
-
-            }
-        });
 
 
         CommentAuctionAdapter = new commentAuctionAdapter(1,commentList);
